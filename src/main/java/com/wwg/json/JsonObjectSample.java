1b7663844f041db4b3760a1c6be23e9a601ca4f3
TomWwg <741889962@qq.com>
2017-12-14 10:38:15 +0800

    使用Map来构建JSON对象
    注意，使用Map构建的JSON对象，在对象值为null的时候也能输出该值；而使用JSONObject方法构建JSON不能

--- src/main/java/com/wwg/json/JsonObjectSample.java
+++ src/main/java/com/wwg/json/JsonObjectSample.java
@@ -3,6 +3,9 @@ package com.wwg.json;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by wwg on 2017/12/14.
  * @author wwg
@@ -11,8 +14,13 @@ public class JsonObjectSample {
 
     public static void main(String[] args) {
         jSONObjectSample();
+        createJsonByMap();
     }
 
+    /**
+     * 使用JSONObject方法构建JSON对象
+     * 注意：value为null的car和house在输出时候没有出现
+     */
     private static void jSONObjectSample() {
         JSONObject wangxiaoer = new JSONObject();
         Object nullObj = null;
@@ -30,6 +38,25 @@ public class JsonObjectSample {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
+
+    /**
+     * 使用Map方法创建JSON对象
+     */
+    private static void createJsonByMap(){
+        Map<String, Object> wangxiaoer = new HashMap<String, Object>(16);
+        Object nullObj = null;
+        wangxiaoer.put("name", "王小二");
+        wangxiaoer.put("age", 23.2);
+        wangxiaoer.put("birthday", "1990-01-01");
+        wangxiaoer.put("school", "蓝翔");
+        wangxiaoer.put("major", new String[]{"理发", "挖掘机"});
+        wangxiaoer.put("has_girlfriend", false);
+        wangxiaoer.put("car", nullObj);
+        wangxiaoer.put("house", nullObj);
+        wangxiaoer.put("comment", "这是一个注释");
+
+        System.out.println(new JSONObject(wangxiaoer).toString());
+    }
+
 }
