eeb5109949a55fb8936b4a20025ffd7c4b30a9e9
TomWwg <741889962@qq.com>
2017-12-14 16:52:13 +0800

    JSONObject
    从文件读取JSON，判断null

--- src/main/java/com/wwg/json/ReadJsonSample.java
+++ src/main/java/com/wwg/json/ReadJsonSample.java
@@ -1,6 +1,7 @@
 package com.wwg.json;
 
 import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,10 +15,27 @@ import java.io.IOException;
 public class ReadJsonSample {
 
     public static void main(String[] args) throws IOException, JSONException {
-        File file = new File(ReadJsonSample.class.getResource("/wangxiaoer.json").getFile());
+        //相对路径的写法出现错误，有待改进
+        File file = new File("E:/wwgProject/jsonstudy/src/main/java/com/wwg/json/wangxiaoer.json");
         //读取文件内容
         String content = FileUtils.readFileToString(file);
         JSONObject jsonObject = new JSONObject(content);
-        System.out.println("姓名是：" + jsonObject.getString("name"));
+        //从文件读取JSON， 判断null
+        if(!jsonObject.isNull("name")){
+            System.out.println("姓名是：" + jsonObject.getString("name"));
+        }
+        if(!jsonObject.isNull("hello")){
+            System.out.println("hello");
+        }
+        if(jsonObject.isNull("hello")){
+            System.out.println("hello is null.");
+        }
+        System.out.println("年龄：" + jsonObject.getDouble("age"));
+        System.out.println("有没有女朋友？" + jsonObject.getBoolean("has_girlfriend"));
+        JSONArray majorArray = jsonObject.getJSONArray("major");
+        for (int i = 0; i < majorArray.length(); i++){
+            String major = (String) majorArray.get(i);
+            System.out.println("专业-" + (i+1) + ":" + major);
+        }
     }
 }
