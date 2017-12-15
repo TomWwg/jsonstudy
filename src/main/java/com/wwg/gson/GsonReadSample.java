f058717bfd069d749cdd03ffc9925c969c3a3005
TomWwg <741889962@qq.com>
2017-12-15 10:09:51 +0800

    JSON学习模块
    JSON构建时候不能使用Date类型的数据，但是GSON可以

--- src/main/java/com/wwg/gson/GsonReadSample.java
+++ src/main/java/com/wwg/gson/GsonReadSample.java
@@ -1,7 +1,9 @@
 package com.wwg.gson;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.wwg.bean.Diaosi;
+import com.wwg.bean.DiaosiWithBirthday;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -21,5 +23,11 @@ public class GsonReadSample {
         Gson gson = new Gson();
         Diaosi wangxiaoer = gson.fromJson(content, Diaosi.class);
         System.out.println(wangxiaoer);
+
+        Gson gsonWihBirthday = new GsonBuilder().setDateFormat("yyyy-mm-dd HH:mm sss").create();
+        DiaosiWithBirthday wangxiaoerWithBirthday = gson.fromJson(content, DiaosiWithBirthday.class);
+        System.out.println(wangxiaoerWithBirthday);
+        //toLocalString方法用于显示当前地区的时间日期格式，不用于计算
+        System.out.println(wangxiaoerWithBirthday.getBirthday().toLocaleString());
     }
 }
